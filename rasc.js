a478c6f7de7f222b65f0912ecc7611e7bd184f98
vivi-toto <fernandavitoria856@gmail.com>
2023-10-28 23:09:23 -0400

    novo arquivo js

---
+++ rasc.js
@@ -0,0 +1,3 @@
+const myString = "MrtyNNgMM";
+const toFind = "M";
+
--- rascunho.js
+++
@@ -1,9 +0,0 @@
-const myString = "MrtyNNgMM";
-const caracterToFind = "M";
-
-if (myString.includes(caracterToFind)) {
-    console.log(`A string contém o caracter que você procura: ${caracterToFind}`);
-    
-} else {
-    console.log(`A string não contém o caracter que você procura: ${caracterToFind}`);
-}
