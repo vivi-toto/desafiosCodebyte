dcacff939c35b78eef12c577286fd56748faa2f6
vivi-toto <fernandavitoria856@gmail.com>
2023-10-28 22:25:07 -0400

    criadas variaveis inicias do problema proposto

--- rascunho.js
+++ rascunho.js
@@ -1,1 +1,9 @@
 const myString = "MrtyNNgMM";
+const caracterToFind = "M";
+
+if (myString.includes(caracterToFind)) {
+    console.log(`A string contém o caracter que você procura: ${caracterToFind}`);
+    
+} else {
+    console.log(`A string não contém o caracter que você procura: ${caracterToFind}`);
+}
